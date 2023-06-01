0224f68696a3e2225410f3c4fa0bbcf4442c1a6e
Claus Aranha <caranha@gmail.com>
2023-06-01 22:13:22 +0900

    Updated Topic 06 -- sample size calculation

--- topic06/rcode/code4slide.R
+++ topic06/rcode/code4slide.R
@@ -19,3 +19,33 @@ power.t.test(n = 20, sd = 2.1, sig.level = 0.05, type = "one.sample", alternativ
 
 ## Checking for sample size
 power.t.test(power = 0.85, sd = 2.1, sig.level = 0.05, type = "one.sample", alternative = "two.sided", delta = 0.5)
+
+## more flexible power_t_test function from the MESS package
+install.packages("MESS") #note: installing a package takes forever
+library(MESS)
+
+## Two samples, equal variance:
+power.t.test(delta = 15, sd = 15, sig.level = 0.05, power = 0.8,
+             type = "two.sample", alternative = "one.sided")
+
+## Unequal variances example 1: Optimal Allocation
+## Consider the previous experiment, but the variance of the second sample is half.
+MESS::power_t_test(n=NULL, sd=15, delta=15, 
+                   ratio=2, sd.ratio=2,
+                   power=0.8, sig.level = 0.05,
+                   type="two.sample", alternative="one.sided")
+
+## Unequal variances example 2: Balanced Design
+## Consider the sample size the same for both treatments
+MESS::power_t_test(n=NULL, sd=15, delta=15, 
+                   ratio=1, sd.ratio=2,
+                   power=0.8, sig.level = 0.05,
+                   type="two.sample", alternative="one.sided")
+
+## Unequal variances example 3: Power Calculation
+## Consider a fixed sample size of 20 and 30 observations
+MESS::power_t_test(n=20, sd=15, delta=15, 
+                   ratio=1.5, sd.ratio=2,
+                   power=NULL, sig.level = 0.05,
+                   type="two.sample", alternative="one.sided")
+
